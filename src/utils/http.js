ec6cc10ed46e9890e96e338ae5ee9fdb73373f68
bekzodjonganiyev <bekzodganiyev471@gmail.com>
2023-07-03 13:40:29 +0500

    baseUrl changed

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,6 +1,6 @@
 export const fetchApi = async (url, data) => {
   try {
-    const res = await fetch(`https://tkti-back-lexde.ondigitalocean.app/${url}`, data);
+    const res = await fetch(`https://dolphin-app-ka7ge.ondigitalocean.app/${url}`, data);
     return res.ok ? res.json() : res;
   } catch ({ message }) {
     return message;
