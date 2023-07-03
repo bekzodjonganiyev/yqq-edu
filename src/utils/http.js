ab0d510d6265ba413e97734f6ff18d8afcb603e9
bekzodjonganiyev <bekzodganiyev471@gmail.com>
2023-07-03 13:52:59 +0500

    wip

--- src/utils/http.js
+++ src/utils/http.js
@@ -2,7 +2,7 @@ export const fetchApi = async (url, data) => {
   try {
     const res = await fetch(`https://dolphin-app-ka7ge.ondigitalocean.app/${url}`, data);
     return res.ok ? res.json() : res;
-  } catch ({ message }) {
+  } catch ({ message }) { 
     return message;
   }
 };
